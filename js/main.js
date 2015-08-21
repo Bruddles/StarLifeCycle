9b25d9d736372a67358002f538a110a374c34840
Brodie Gilmour <brodie.gilmour@webappuk.com>
2015-08-21 17:17:09 +0100

    Made Three.js responsible for the star image. Updates size and colour.

--- js/main.js
+++ js/main.js
@@ -13,48 +13,82 @@ require.config({
     }
 });
 require(['stars', 'jQuery', 'THREE'], function(stars, $, THREE){
-  var star = new stars.MainSequenceStar(1);
-  updateView(star);
+  var star = new stars.MainSequenceStar(1),
+      scene = new THREE.Scene(),
+      camera,
+      renderer,
+      geometry,
+      material,
+      sphere;
+      
+  createStarView($('.starImageContainer'), star);
+  updateStarView(star);
   
   $('#starMassSlider').change(function(){
     star.calculateProperties($(this).val());
-    updateView(star);
+    updateStarView(star);
   });
   
-  createSphere();
-  
-  function createSphere(){
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, 
-      $('.starImageContainer').css('width').slice(0, -2) / $('.starImageContainer').css('height').slice(0, -2), 
+  function createStarView(container, star){
+    var width = $(container).css('width').slice(0, -2),
+        height = $(container).css('height').slice(0, -2);
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera(75, 
+      width / height, 
       0.1, 
       1000);
+    camera.position.z = 8;
     
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize($('.starImageContainer').css('width').slice(0, -2), $('.starImageContainer').css('height').slice(0, -2));
-    $('body').append(renderer.domElement);
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize(width, height);
+    $(container).append(renderer.domElement);
     
-    var geometry = new THREE.SphereGeometry(0.5, 32, 32);
-    var material = new THREE.MeshBasicMaterial({color: 0xffff00});
-    var sphere = new THREE.Mesh(geometry, material);
+    geometry = new THREE.SphereGeometry(star.solarRadius , 32, 32);
+    material = new THREE.MeshBasicMaterial({color: star.solarClass});
+    sphere = new THREE.Mesh(geometry, material);
     scene.add(sphere);
+    
+    sphere.geometry.dynamic = true;
+    sphere.geometry.verticesNeedUpdate = true;
+    sphere.geometry.normalsNeedUpdate = true;
   }
   
-  function updateView(star){
+  function animateStarView(){
+    requestAnimationFrame(animateStarView);
+    renderer.render(scene, camera);
+  }
+  
+  function updateStarView(star){
+    var scale = star.solarRadius,
+        rgb = hexToRGB(star.solarClass);
+        
+    console.log('hex: ' + star.solarClass);
+    console.log('r: ' + rgb.r + ' g: ' + rgb.g + ' b: ' + rgb.b);
     $('#starMassVariable').text(star.solarMass);
     $('#starRadiusVariable').text(star.solarRadius);
     $('#starLuminosityVariable').text(star.solarLuminosity);
     $('#starTemperatureVariable').text(star.temperature);
+    
+    sphere.scale.x = scale;
+    sphere.scale.y = scale;
+    sphere.scale.z = scale;  
+    
+    sphere.material.color.r = rgb.r;
+    sphere.material.color.g = rgb.g;
+    sphere.material.color.b = rgb.b;
+    
+    animateStarView();
   
-    $('.starImage').css('background-color', star.solarClass );
-    $('.starImage').css('height', function(){
-      var width = $('.starImageContainer').css('height').slice(0, -2);
-      return ((star.solarRadius / 6) * width);  
-    });
-    $('.starImage').css('width', function(){
-      var width = $('.starImageContainer').css('height').slice(0, -2);
-      return ((star.solarRadius / 6) * width);  
-    });
+    // $('.starImage').css('background-color', star.solarClass );
+    
+    function hexToRGB(hex){
+      hex = (hex.charAt(0)=="#") ? hex.substring(1,7):hex;
+      return {
+        r: parseInt((hex).substring(0,2),16),
+        g: parseInt((hex).substring(2,4),16),
+        b: parseInt((hex).substring(4,6),16)
+      }
+    }
   }
 });
 
